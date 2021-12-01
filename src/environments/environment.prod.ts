948942c16350ac0b0a7db57f2b2c88911958f2ba
jorgeCastro <jorgeluiscastrov5@gmail.com>
2021-12-01 16:09:35 -0400

    deploy ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+	production: true,
+	MAPBOX: 'pk.eyJ1IjoiYXJtYWdlZG9uIiwiYSI6ImNrcDhreXBycDAycnUyd3A0anA3dm00eXQifQ.Ah8m9bt7fXdX9Ut-PmKBLg',
+	dialogflow: {
+		chatBot: '',
+	},
 };
