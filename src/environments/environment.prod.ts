b458ba8e420068f55cbbbe2b37bfc16580facb17
herlanjavier <castroherlan@gmail.com>
2021-12-07 07:46:24 -0400

    cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  MAPBOX: 'pk.eyJ1IjoiYXJtYWdlZG9uIiwiYSI6ImNrcDhreXBycDAycnUyd3A0anA3dm00eXQifQ.Ah8m9bt7fXdX9Ut-PmKBLg',
+  MAPBOX:
+    'pk.eyJ1IjoiYXJtYWdlZG9uIiwiYSI6ImNrcDhreXBycDAycnUyd3A0anA3dm00eXQifQ.Ah8m9bt7fXdX9Ut-PmKBLg',
   dialogflow: {
     chatBot: '',
   },
-
-  backURL: 'https://15b1-2800-cd0-2a1c-2000-8077-843e-ecd7-a2fc.ngrok.io/api'
+  backURL: 'https://1a9a-2800-cd0-2a1c-2000-684d-76e7-f3dc-52fa.ngrok.io/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,12 @@
 // local: backURL: 'http://127.0.0.1:8000/api'
 export const environment = {
   production: true,
-  MAPBOX: 'pk.eyJ1IjoiYXJtYWdlZG9uIiwiYSI6ImNrcDhreXBycDAycnUyd3A0anA3dm00eXQifQ.Ah8m9bt7fXdX9Ut-PmKBLg',
+  MAPBOX:
+    'pk.eyJ1IjoiYXJtYWdlZG9uIiwiYSI6ImNrcDhreXBycDAycnUyd3A0anA3dm00eXQifQ.Ah8m9bt7fXdX9Ut-PmKBLg',
   dialogflow: {
     chatBot: '',
   },
-  backURL: 'https://15b1-2800-cd0-2a1c-2000-8077-843e-ecd7-a2fc.ngrok.io/api'
+  backURL: 'https://1a9a-2800-cd0-2a1c-2000-684d-76e7-f3dc-52fa.ngrok.io/api',
 };
 
 /*
